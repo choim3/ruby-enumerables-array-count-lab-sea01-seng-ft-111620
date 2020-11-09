b25fadc6b4c9702a86eaadfd81b3e399e0fdd744
Matt Choi <choim3@spu.edu>
2020-11-09 00:40:10 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,8 +1,14 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   
+<<<<<<< HEAD
  return array.count {|total_strings| total_strings.class == String}
   
+=======
+ return array.count {|string_amount| array.class == String}
+  
+ # return array
+>>>>>>> 9e4f367596ef993c400615f8df1e5a8282517449
 
 end
 
